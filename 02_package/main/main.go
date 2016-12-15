3b6a1b5094e7158dfd59459eb834436ab1ef1cc1
liangxu chen <liangxu chen>
2016-12-15 11:55:52 +0800

    02 package

---
+++ 02_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/UdemyTraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG,olleh"))
+	fmt.Println(stringutil.MyName)
+}
