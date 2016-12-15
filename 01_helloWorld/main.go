dce221a9951cd2da86c0a9e748b3ede4082ed7ad
liangxu chen <liangxu chen>
2016-12-15 11:02:57 +0800

    lec 2

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -4,5 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
-	fmt.Println("second hello")
+	fmt.Printf("%d -%b -%x \n", 42, 42, 42)
+	fmt.Printf("%d -%b -%#X \n", 42, 42, 42)
+
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %x  \n", i, i, i)
+	}
 }
