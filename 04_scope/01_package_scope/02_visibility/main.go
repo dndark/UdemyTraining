79adc76873bfb18c9e8716f366e94d53d3285851
liangxu chen <liangxu chen>
2016-12-17 09:10:33 +0800

    03 variable

--- 04_scope/01_package_scope/02_visibility/main.go
+++ 04_scope/01_package_scope/02_visibility/main.go
@@ -1,9 +1,11 @@
 package main
 
-<<<<<<< Updated upstream
-//this is file level scope. each file need to import it
-import "fmt"
-import "github.com/04_scope/01_package_scope/01"
+import (
+	"github.com/UdemyTraining/04_scope/01_package_scope/01"
+	"fmt"
+)
+
+
 func max(x int) int {
 	max := 42 + x
 	return max
@@ -11,22 +13,6 @@ func max(x int) int {
 
 func main() {
 	max := max(7)
-    _1.PrintVar()
+   	 _1.PrintVar()
 	fmt.Println(max)
 }
-=======
-import "fmt"
-
-func main(){
-	x:=42
-	fmt.Println(x)
-	foo()
-}
-
-func foo(){
-	fmt.Println(y)
-}
-
-//this is in the out scope so foo can call this varaible
-var y = 42
->>>>>>> Stashed changes
